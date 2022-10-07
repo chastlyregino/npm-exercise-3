9aac214c2ea496055d10e1b7ef585e64936b4367
Chastly Regino <chastly_regino@icloud.com>
2022-10-07 11:42:03 +0800

    Created a program using expression-parser with documentation in the README.md file

---
+++ index.js
@@ -0,0 +1,8 @@
+const { parse, resolve, toString, toArray } = require('@estilles/expression-parser');
+const { argv } = require('node:process');
+
+if (argv.length != 3) {
+    console.log(`This program only accepts a SINGLE arguement`);
+} else {
+   console.log(`The postfix expression is: ${toString(parse(argv[2]))}`);
+}

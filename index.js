24c7aafb321cd3dc96aae8775684c26a828fad02
Chastly Regino <chastly_regino@icloud.com>
2022-10-11 09:19:14 +0800

    Implemented prettier into index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { parse, toString } = require('@estilles/expression-parser');
 const { argv } = require('node:process');
 
 if (argv.length !== 3) {
-    console.log(`This program only accepts a SINGLE arguement`);
+  console.log(`This program only accepts a SINGLE arguement`);
 } else {
-   console.log(`The postfix expression is: ${toString(parse(argv[2]))}`);
+  console.log(`The postfix expression is: ${toString(parse(argv[2]))}`);
 }
